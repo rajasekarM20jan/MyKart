19334e3756b354b538fc5200b84266f40f78e15a
Temp-user4 <rs20jan1998s@gmail.com>
2022-10-20 12:59:09 +0530

    init

--- app/src/main/java/controller/controllerLogin.java
+++ app/src/main/java/controller/controllerLogin.java
@@ -11,7 +11,7 @@ public class controllerLogin {
      this.userVerification=mainActivity;
  }
  public void verifyData(String userMobile,String userMail,String password,String editTextMail,String editTextPass){
-     if(editTextMail.equals(userMobile) || editTextMail.equals(userMail)){
+     if(editTextMail.equals(userMobile)){
       if(editTextPass.equals(password)){
        SharedPreferences sp= userVerification.getSharedPreferences("MyPref", Context.MODE_PRIVATE);
        SharedPreferences.Editor editor=sp.edit();
